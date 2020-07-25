7e82eebe44ff722095eea477184fbd424c2ac2b0
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-07-25 13:30:13 +0900

    Update MCMF.cpp

--- Graph/MCMF.cpp
+++ Graph/MCMF.cpp
@@ -1,90 +1,128 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
+ 
 using namespace std;
-const int MAX_N = 100; // 최대 N, M
-const int MAX_V = 2*(MAX_N+1); // 최대 정점 개수
-const int S = MAX_V-2; // 소스 정점 번호
-const int E = MAX_V-1; // 싱크 정점 번호
-const int INF = 1000000000;
  
-int main(){
-    // 정점 번호: 0~MAX_N: 서점, MAX_N~MAX_N*2: 사람
-    int N, M;
-    int c[MAX_V][MAX_V] = {0}; // 각 간선의 용량
-    int d[MAX_V][MAX_V] = {0}; // 각 간선의 비용
-    int f[MAX_V][MAX_V] = {0}; // 각 간선에 흐르는 중인 유량
-    vector<int> adj[MAX_V]; // 각 정점의 인접 리스트
+const int MAX_V = 900;
+const int S = MAX_V - 2;
+const int T = MAX_V - 1;
+const int WORK = 400;
+const int INF = 987654321;
  
-    scanf("%d %d", &N, &M);
-    // 각 사람 정점과 싱크 정점 사이 간선 추가 (비용 0)
-    for(int i=MAX_N; i<MAX_N+N; i++){
-        cin>>c[i][E];
-        adj[E].push_back(i);
-        adj[i].push_back(E);
-    }
-    // 소스 정점과 각 서점 정점 사이 간선 추가 (비용 0)
-    for(int i=0; i<M; i++){
-        cin>>c[S][i];
+vector<int> adj[MAX_V];
+int c[MAX_V][MAX_V];
+int f[MAX_V][MAX_V];
+int d[MAX_V][MAX_V];
+ 
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+ 
+    // S :: 898   T :: 899   직원 :: 1 ~ 400   일 :: 401 ~ 800
+ 
+    // S와 직원을 연결
+    for (int i = 1; i <= n; i++)
+    {
+        c[S][i] = 1;
+ 
         adj[S].push_back(i);
         adj[i].push_back(S);
     }
-    // 서점과 사람 사이 간선 추가 (비용 C_ij)
-    for(int i=0; i<M; i++){
-        for(int j=MAX_N; j<MAX_N+N; j++){
-            cin>>d[i][j];
-            d[j][i] = -d[i][j]; // 역방향 간선의 비용: 순방향의 -1배
-            c[i][j] = INF; // 순방향 간선만 용량이 1 이상
-            adj[i].push_back(j);
-            adj[j].push_back(i);
+ 
+    // T와 일을 연결
+    for (int i = 1; i <= m; i++)
+    {
+        c[i + WORK][T] = 1;
+ 
+        adj[i + WORK].push_back(T);
+        adj[T].push_back(i + WORK);
+    }
+ 
+    // 입력값 처리
+    for (int i = 1; i <= n; i++)
+    {
+        int wNum;
+        cin>>wNum;
+        for (int j = 0; j < wNum; j++)
+        {
+            int workNo, cost;
+            cin>>workNo>>cost;
+            adj[i].push_back(workNo + WORK);
+            adj[workNo + WORK].push_back(i);
+ 
+            d[i][workNo + WORK] = cost;
+            d[workNo + WORK][i] = -cost;
+ 
+            c[i][workNo + WORK] = 1;
         }
     }
  
-    int result = 0; // 최소 비용
-    // MCMF 시작
-    while(1){
+    int result = 0;
+    int cnt = 0;
+ 
+    while (1)
+    {
         int prev[MAX_V], dist[MAX_V];
-        bool inQ[MAX_V] = {0}; // 해당 정점이 큐 안에 있는가?
-        queue<int> Q;
-        fill(prev, prev+MAX_V, -1);
-        fill(dist, dist+MAX_V, INF);
+        bool inQ[MAX_V] = { 0 };
+ 
+        // SPFA
+        queue<int> q;
+        fill(prev, prev + MAX_V, -1);
+        fill(dist, dist + MAX_V, INF);
+ 
         dist[S] = 0;
         inQ[S] = true;
-        Q.push(S);
  
-        while(!Q.empty()){
-            int curr = Q.front();
-            Q.pop();
-            inQ[curr] = false; // 큐에서 꺼냄
-            for(int next: adj[curr]){
-                // 최단 경로를 찾는 중이지만, 여전히 여유 용량 있어야 함
-                if(c[curr][next]-f[curr][next] > 0 && dist[next] > dist[curr]+d[curr][next]){
-                    dist[next] = dist[curr] + d[curr][next];
-                    prev[next] = curr;
-                    // 큐에 들어있지 않다면 큐에 넣음
-                    if(!inQ[next]){
-                        Q.push(next);
+        q.push(S);
+ 
+        while (!q.empty())
+        {
+            int here = q.front();
+            q.pop();
+ 
+            inQ[here] = false;
+ 
+            for (int i = 0; i < adj[here].size(); i++)
+            {
+                int next = adj[here][i];
+                if (c[here][next] - f[here][next] > 0 && dist[next] > dist[here] + d[here][next])
+                {
+                    dist[next] = dist[here] + d[here][next];
+                    prev[next] = here;
+                    if (!inQ[next])
+                    {
+                        q.push(next);
                         inQ[next] = true;
                     }
                 }
+ 
             }
         }
-        // 더 이상 경로가 없다면 루프 탈출
-        if(prev[E] == -1) break;
  
-        // 경로상에서 가장 residual이 작은 간선을 찾아 최대 흘릴 수 있는 flow 찾음
+        if (prev[T] == -1)
+            break;
+ 
+        // 최대 유량
         int flow = INF;
-        for(int i=E; i!=S; i=prev[i])
+ 
+        for (int i = T; i != S; i = prev[i])
             flow = min(flow, c[prev[i]][i] - f[prev[i]][i]);
  
-        // 경로상의 모든 간선에 flow만큼의 유량을 흘림
-        for(int i=E; i!=S; i=prev[i]){
-            result += flow * d[prev[i]][i]; // 총 비용이 각 간선 비용만큼 증가
+        for (int i = T; i != S; i = prev[i])
+        {
+            result += flow * d[prev[i]][i];
             f[prev[i]][i] += flow;
             f[i][prev[i]] -= flow;
         }
+ 
+        cnt++;
     }
-    // 정답 출력
-    cout<<result;
+    cout<<cnt<<"\n"<<result;
+    return 0;
 }
+
+
+출처: https://www.crocus.co.kr/1090 [Crocus]
