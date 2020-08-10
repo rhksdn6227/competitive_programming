3ac8297cb5c042af15b750366b79133430942354
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-08-10 21:19:39 +0900

    Update lazy_propogation.cpp

--- Tree/lazy_propogation.cpp
+++ Tree/lazy_propogation.cpp
@@ -1,15 +1,12 @@
 int N, M, K;
 
 struct seg {
-	lint a[2100000], lz[2100000], lim;
+	vl item, lz;
+	int half;
 	void init() {
-		for (lim = 1; lim < N; lim <<= 1);
-		memset(a, 0, sizeof(a));
-		memset(lz, 0, sizeof(lz));
-		for (int i = 0; i < N; i++)
-			cin >> a[i + lim];
-		for (int i = lim - 1; i; i--)
-			a[i] = a[i * 2] + a[i * 2 + 1];
+		for (half = 1; half < N; half <<= 1);
+		item.resize(half*2);
+		lz.resize(half*2);
 	}
 	void lazy(int n, int nl, int nr) {
 		if (lz[n]) {
@@ -21,7 +18,7 @@ struct seg {
 			lz[n] = 0;
 		}
 	}
-	void update(lint l, lint r, lint diff) { update(l, r, 1, 0, lim - 1, diff); };
+	void update(lint l, lint r, lint diff) { update(l, r, 1, 0, half - 1, diff); };
 	void update(lint l, lint r, int n, int nl, int nr, lint diff) {
 		lazy(n, nl, nr);
         if (r < nl || nr < l) return;
@@ -33,14 +30,14 @@ struct seg {
 		int mid = (nl + nr) / 2;
 		update(l, r, n * 2, nl, mid, diff);
 		update(l, r, n * 2 + 1, mid+1, nr, diff);
-		a[n] = a[n * 2] + a[n * 2 + 1];
+		item[n] = item[n * 2] + item[n * 2 + 1];
 	}
-	lint query(lint l, lint r) { return query(l, r, 1, 0, lim - 1); }
+	lint query(lint l, lint r) { return query(l, r, 1, 0, half - 1); }
 	lint query(lint l, lint r, int n, int nl, int nr) {
 		lazy(n, nl, nr);
 		if (r < nl || nr < l) return 0;
-		if (l <= nl && nr <= r) return a[n];
+		if (l <= nl && nr <= r) return item[n];
 		int mid = (nl + nr) / 2;
 		return query(l, r, n * 2, nl, mid) + query(l, r, n * 2 + 1, mid + 1, nr);
 	}
-}seg;
+};
