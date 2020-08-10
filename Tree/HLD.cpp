d698d63e9e62c350f6edcec846317422ae2816f0
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-08-10 20:04:33 +0900

    Update HLD.cpp

--- Tree/HLD.cpp
+++ Tree/HLD.cpp
@@ -1,189 +1,98 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <functional>
-#include <stack>
-#include <queue>
-#include <cstdlib>
-#include <cstring>
-#include <halfits.h>
-#include <map>
-#include <set>
-#include <deque>
-#include <complex>
-#define all(v) (v).begin(),(v).end()
-#define sz(v) ((v).size())
-using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vl = vector<ll>;
-using pdd = pair<double, double>;
-using pil = pair<int, ll>;
+int n;
+vi adj[100001];
 
-struct seg {
-	int item[270000], half;
-	void init(int n) {
-		for (half = 1; half <= n; half <<= 1);
+struct segtree {
+
+	int n, half;
+	vi item;
+
+	segtree(int rn) {
+		n = rn;
+		for (half = 1; half < n; half <<= 1);
+		item.resize(half * 2);
 	}
-	void update(int x, int v) {
-		x += half;
-		item[x] = v;
-		while (x > 1) {
-			x >>= 1;
-			item[x] = max(item[2 * x], item[2 * x + 1]);
+	void init(vector<int>& arr) {
+		for (int i = 0; i < n; i++)
+			item[i + half] = arr[i];
+		for (int i = n; i < half; i++)
+			item[i + half] = 0;
+		for (int i = half - 1; i > 0; i--)
+			item[i] = item[i * 2] + item[i * 2 + 1];
+	}
+	void update(int idx, int val) {
+		idx += half;
+		item[idx] = val;
+		while (idx /= 2) {
+			item[idx] = max(item[idx * 2], item[idx * 2 + 1]);
 		}
 	}
-	int query(int s, int e) {
-		s += half;
-		e += half;
-		int ret = -1e9;
-		while (s <= e) {
-			if (s % 2 == 1) ret = max(ret, item[s++]);
-			if (e % 2 == 0) ret = max(ret, item[e--]);
-			s >>= 1;
-			e >>= 1;
+	int query(int l, int r) {
+		l += half;
+		r += half;
+		int ret = 0;
+		while (l <= r) {
+			if (l % 2)
+				ret = max(ret, item[l++]);
+			if (!(r % 2))
+				ret = max(ret, item[r--]);
+			l >>= 1, r >>= 1;
 		}
 		return ret;
 	}
-}seg;
+};
 
-typedef pair<int, int> p;
-
-int n, m;
-int depth[101010], lcaT[20][101010], par[101010], weight[101010];
-int comp[101010], head[101010], lab[101010];
-int piv1, piv2;
-vector<p> g[101010];
-int newCost[101010];
-int s[101010], e[101010], c[101010];
-
-void dfs(int now, int prv) {
-	weight[now] = 1;
-	for (auto& i : g[now]) {
-		int nxt = i.first, cost = i.second;
-		if (nxt == prv) continue;
-		depth[nxt] = depth[now] + 1;
-		lcaT[0][nxt] = now;
-		par[nxt] = now;
-		newCost[nxt] = cost;
-		dfs(nxt, now);
-		weight[now] += weight[nxt];
-	}
-}
-
-void hld(int now, int prv) {
-	sort(g[now].begin(), g[now].end(), [](const p& a, const p& b) {
-		return weight[a.first] > weight[b.first];
-		});
-
-	int heavy = -1;
-	for (auto& i : g[now]) {
-		int nxt = i.first;
-		if (nxt == prv) continue;
-		heavy = nxt;
-		comp[nxt] = comp[now];
-		lab[nxt] = ++piv2;
-		hld(nxt, now);
-		break;
+struct hld {
+	vi par, dep, heavy, head, pos; 
+	//par[i]:i의 부모  dep[i]:i의 트리상 깊이  heavy[i]:노드i의 자식들 중 가장 무거운쪽 자식
+	//head[i]:i노드가 속한 체인에 속한 노드중 제일 깊이 얕은 놈 pos[i]:노드i의 decompose된 상태의 인덱스
+	int cur_pos = 0;
+	
+	hld(int rn) {
+		par.resize(rn + 1);
+		dep.resize(rn + 1);
+		heavy.resize(rn + 1);
+		head.resize(rn + 1);
+		pos.resize(rn + 1);
+		fill(all(heavy), -1);
 	}
 
-	for (auto& i : g[now]) {
-		int nxt = i.first;
-		if (nxt == heavy || nxt == prv) continue;
-		comp[nxt] = ++piv1;
-		head[piv1] = nxt;
-		lab[nxt] = ++piv2;
-		hld(nxt, now);
-	}
-}
-
-int lca(int s, int e) {
-	if (depth[s] > depth[e]) swap(s, e);
-	int diff = depth[e] - depth[s];
-	for (int i = 0; i < 20; i++) {
-		if ((diff >> i) & 1) e = lcaT[i][e];
-	}
-	for (int i = 19; i >= 0; i--) {
-		if (lcaT[i][s] != lcaT[i][e]) s = lcaT[i][s], e = lcaT[i][e];
-	}
-	if (s == e) return s;
-	return lcaT[0][s];
-}
-
-void update(int idx, int val) {
-	if (depth[s[idx]] > depth[e[idx]]) swap(s[idx], e[idx]);
-	seg.update(lab[e[idx]], val);
-}
-
-int sub_query(int s, int e) {
-	int ret = 0;
-	while (comp[s] != comp[e]) {
-		int ss = head[comp[s]], ee = head[comp[e]];
-		if (depth[ss] > depth[ee]) {
-			ret = max(ret, seg.query(lab[ss], lab[s]));
-			s = par[ss];
+	int dfs(int cur) { //par,dep,heavy를 알아낸다
+		int ret = 1, mx = 0;
+		for (int next : adj[cur]) {
+			if (next == par[cur]) continue;
+			par[next] = cur, dep[next] = dep[cur] + 1;
+			int next_sz = dfs(next);
+			ret += next_sz;
+			if (mx < next_sz)
+				mx = next_sz, heavy[cur] = next;
 		}
-		else {
-			ret = max(ret, seg.query(lab[ee], lab[e]));
-			e = par[ee];
+		return ret;
+	}
+	void decompose(int cur, int top) { //head,pos를 알아낸다
+		head[cur] = top, pos[cur] = cur_pos++;
+		if (heavy[cur] != -1)
+			decompose(heavy[cur], top);
+
+		for (int next : adj[cur]) {
+			if (next != par[cur] && next != heavy[cur])
+				decompose(next, next);
 		}
 	}
-	if (depth[e] > depth[s]) swap(s, e);
-	ret = max(ret, seg.query(lab[e] + 1, lab[s]));
-	return ret;
-}
-
-int query(int u, int v) {
-	int t = lca(u, v);
-	return max(sub_query(u, t), sub_query(v, t));
-}
-
-
-
-int main() {
-	/*#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif*/
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	cin >> n;
-	seg.init(n);
-	for (int i = 1; i < n; i++) {
-		cin >> s[i] >> e[i] >> c[i];
-		g[s[i]].push_back({ e[i], c[i] });
-		g[e[i]].push_back({ s[i], c[i] });
-	}
-
-	dfs(1, 0);
-	for (int i = 1; i < 20; i++) {
-		for (int j = 1; j <= n; j++) {
-			lcaT[i][j] = lcaT[i - 1][lcaT[i - 1][j]];
+	int query(int l, int r,segtree& seg) {
+		int ret = 0;
+		for (; head[l] != head[r]; r = par[head[r]]) {
+			if (dep[head[l]] > dep[head[r]]) swap(l, r);
+			int tmp = seg.query(pos[head[r]], pos[r]);
+			ret = max(ret, tmp);
 		}
-	}
-
-	head[1] = comp[1] = lab[1] = piv1 = piv2 = 1;
-	hld(1, 0);
-	for (int i = 2; i <= n; i++) {
-		seg.update(lab[i], newCost[i]);
-	}
-	cin >> m;
-	while (m--) {
-		int op; cin >> op;
-		if (op == 1) {
-			int idx, val; cin >> idx >> val;
-			update(idx, val);
+		if (dep[l] > dep[r]) swap(l, r);
+		if (pos[l] != pos[r]) {
+			int tmp = seg.query(pos[l] + 1, pos[r]);
+			ret = max(ret, tmp);
 		}
-		else {
-			int u, v; cin >> u >> v;
-			cout << query(u, v) << "\n";
-		}
+		return ret;
 	}
-}
-
-//출처:https://justicehui.github.io/hard-algorithm/2019/02/06/HLD/
+	void update(int i, int val,segtree& seg) {
+		seg.update(pos[i], val);
+	}
+};
