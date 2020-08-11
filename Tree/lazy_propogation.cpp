4be2bd30f90506440cf90a95d3f4daa09c345911
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-08-11 16:41:41 +0900

    Update lazy_propogation.cpp

--- Tree/lazy_propogation.cpp
+++ Tree/lazy_propogation.cpp
@@ -1,43 +1,41 @@
-int N, M, K;
-
-struct seg {
+struct segtree {
 	vl item, lz;
 	int half;
 	void init() {
-		for (half = 1; half < N; half <<= 1);
-		item.resize(half*2);
-		lz.resize(half*2);
+		for (half = 1; half < n; half <<= 1);
+		item.resize(half * 2);
+		lz.resize(half * 2);
 	}
-	void lazy(int n, int nl, int nr) {
-		if (lz[n]) {
-			a[n] += lz[n] * (nr - nl + 1);
-			if (n < lim) {
-				lz[n * 2] += lz[n];
-				lz[n * 2 + 1] += lz[n];
+	void lazy(int node, int nl, int nr) {
+		if (lz[node]) {
+			item[node] += lz[node] * (nr - nl + 1);
+			if (node < half) {
+				lz[node * 2] += lz[node];
+				lz[node * 2 + 1] += lz[node];
 			}
-			lz[n] = 0;
+			lz[node] = 0;
 		}
 	}
-	void update(lint l, lint r, lint diff) { update(l, r, 1, 0, half - 1, diff); };
-	void update(lint l, lint r, int n, int nl, int nr, lint diff) {
-		lazy(n, nl, nr);
-        if (r < nl || nr < l) return;
-        if (l <= nl && nr <= r){
-			lz[n] += diff;
-            lazy(n, nl, nr);
-            return;
-        }       
+	void update(ll l, ll r, ll diff) { update(l, r, 1, 0, half - 1, diff); };
+	void update(ll l, ll r, int node, int nl, int nr, ll diff) {
+		lazy(node, nl, nr);
+		if (r < nl || nr < l) return;
+		if (l <= nl && nr <= r) {
+			lz[node] += diff;
+			lazy(node, nl, nr);
+			return;
+		}
 		int mid = (nl + nr) / 2;
-		update(l, r, n * 2, nl, mid, diff);
-		update(l, r, n * 2 + 1, mid+1, nr, diff);
-		item[n] = item[n * 2] + item[n * 2 + 1];
+		update(l, r, node * 2, nl, mid, diff);
+		update(l, r, node * 2 + 1, mid + 1, nr, diff);
+		item[node] = item[node * 2] + item[node * 2 + 1];
 	}
-	lint query(lint l, lint r) { return query(l, r, 1, 0, half - 1); }
-	lint query(lint l, lint r, int n, int nl, int nr) {
-		lazy(n, nl, nr);
+	ll query(ll l, ll r) { return query(l, r, 1, 0, half - 1); }
+	ll query(ll l, ll r, int node, int nl, int nr) {
+		lazy(node, nl, nr);
 		if (r < nl || nr < l) return 0;
-		if (l <= nl && nr <= r) return item[n];
+		if (l <= nl && nr <= r) return item[node];
 		int mid = (nl + nr) / 2;
-		return query(l, r, n * 2, nl, mid) + query(l, r, n * 2 + 1, mid + 1, nr);
+		return query(l, r, node * 2, nl, mid) + query(l, r, node * 2 + 1, mid + 1, nr);
 	}
-};
+}seg;
