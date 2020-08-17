9ef3d6a1e8af40e0b6a1658a9412dfd7995935af
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-08-17 19:48:02 +0900

    Update hard_trie.cpp

--- String/hard_trie.cpp
+++ String/hard_trie.cpp
@@ -1,1 +1,45 @@
-//가족한테 보내달라 
+using pci=pair<char,int>;
+
+struct node {
+	bool finish;
+    vector<pci > next;
+	node() :finish(false), next(vector<pci >()) {}
+    int find_(char c){
+        for (auto &tmp : next)
+            if (tmp.first == c)
+                return tmp.second;
+        return -1;
+    }
+};
+struct trie {
+	int size;
+	vector<node> sub;
+	trie() :size(1) {
+		sub.push_back(node());
+	}
+	void insert(string& str) {
+		int idx = 0;
+		for (int i = 0; i < str.size(); ++i) {
+            int id = sub[idx].find_(str[i]);
+            if(id == -1){
+                sub.push_back(node());
+                sub[idx].next.push_back(pci(str[i], size));
+                idx = size++;
+            }
+			else {
+				idx = id;
+			}
+		}
+		sub[idx].finish = true;
+	}
+	bool search(string& str) {
+		int idx = 0;
+		for (int i = 0; i < str.size(); ++i) {
+			int id = sub[idx].find_(str[i]);
+            if(id == -1)
+                return false;
+			idx = id;
+		}
+		return sub[idx].finish;
+	}
+};
