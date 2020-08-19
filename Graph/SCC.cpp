fa36b3a6503495125f42375bfe8b17d701fa8b33
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-08-19 12:56:58 +0900

    Update SCC.cpp

--- Graph/SCC.cpp
+++ Graph/SCC.cpp
@@ -1,29 +1,28 @@
-vector<vector<int>> scc;
-int v, e;
+vector<vi> scc;
 vector<int> adj[10001];
 int dfsn[10001];
 int finish[10001];
 int idx = 1;
 stack<int> st;
 
-int dfs(int vertex) {
-	dfsn[vertex] = idx++;
-	st.push(vertex);
-	int ret = dfsn[vertex];
-	for (int next : adj[vertex]) {
+int dfs(int now) {
+	dfsn[now] = idx++;
+	st.push(now);
+	int ret = dfsn[now];
+	for (int next : adj[now]) {
 		if (!dfsn[next])
 			ret = min(ret, dfs(next));
 		else if (!finish[next])
 			ret = min(ret, dfsn[next]);
 	}
-	if (dfsn[vertex] == ret) {
+	if (dfsn[now] == ret) {
 		vector<int> vc;
 		while (1) {
 			int tmp = st.top();
 			st.pop();
 			finish[tmp] = 1;
 			vc.push_back(tmp);
-			if (tmp == vertex)
+			if (tmp == now)
 				break;
 		}
 		scc.push_back(vc);
