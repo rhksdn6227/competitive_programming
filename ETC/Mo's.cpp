dfc76021f6af46323e41eca3d7b01e6a046b1120
rhksdn6227 <46395794+rhksdn6227@users.noreply.github.com>
2020-09-29 17:15:19 +0900

    Update Mo's.cpp

--- ETC/Mo's.cpp
+++ ETC/Mo's.cpp
@@ -1,13 +1,14 @@
 //시간복잡도: O((n+m)*sqrt(n))
 int n, m, rt;
 int arr[100001];
-int cnt[1000001];
+int cnt[200001];
 int ans[100001];
+int cmax[100001];
 int cur = 0;
 
 //쿼리의 오른쪽 끝이 속한 블록이 증가하는 순서대로,
 //블록이 같으면 왼쪽 끝이 증가하는 순서로 정렬
-struct query { 
+struct query {
 	int l, r, idx;
 	bool operator<(const query& o) {
 		int x = r / rt;
@@ -18,12 +19,15 @@ struct query {
 
 void make(int idx, bool add) {
 	if (add) {
-		if (cnt[arr[idx]]++ == 0)
-			cur++;
+		cmax[cnt[arr[idx]]]--;
+		cnt[arr[idx]]++;
+		cmax[cnt[arr[idx]]]++;
+		cur = max(cur, cnt[arr[idx]]);
 	}
 	else {
-		if (--cnt[arr[idx]] == 0)
-			cur--;
+		cmax[cnt[arr[idx]]]--;
+		--cnt[arr[idx]];
+		cmax[cnt[arr[idx]]]++;
 	}
 }
 
@@ -34,29 +38,29 @@ int main() {
 	#endif*/
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
-	
-	cin >> n;
+
+	cin >> n >> m;
 	rt = (int)sqrt(n);
-	F(i, 1, n)
+	F(i, 1, n) {
 		cin >> arr[i];
-	cin >> m;
+	}
 	F(i, 1, m) {
 		int a, b;
 		cin >> a >> b;
 		q[i] = { a,b,i };
 	}
 	sort(q + 1, q + 1 + m);
-	int left = 1, right = 0;
-	F(i, 1, m) { //l>r이 되는 경우가 생길 수 있기 때문에 범위 확장 연산 먼저 해준다.
-		while (q[i].l < left)
-			make(--left, true);
-    while (q[i].r > right)
-			make(++right, true);
-		while (q[i].l > left)
-			make(left++, false);
-		while (q[i].r < right)
-			make(right--, false);
-		
+	int left = q[1].l, right = q[1].r;
+	F(i, left, right)
+		make(i, true);
+	ans[q[1].idx] = cur;
+	F(i, 2, m) { //l>r이 되는 경우가 생길 수 있기 때문에 범위 확장 연산 먼저 해준다.
+		while (q[i].l < left) make(--left, true);
+		while (q[i].r > right) make(++right, true);
+		while (q[i].l > left) make(left++, false);
+		while (q[i].r < right) make(right--, false);
+		while (cmax[cur] == 0)
+			cur--;
 		ans[q[i].idx] = cur;
 	}
 	F(i, 1, m)
